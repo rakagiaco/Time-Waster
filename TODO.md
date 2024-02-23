efd15c8d6936ffbac880c03c914e3a9aff7239e6
= <cjmoshy12@gmail.com>
2024-02-22 22:17:45 -0800

    stable 'shell' of game

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Main Stuff
+- load all assets
+- program base world with tilemap
+- add all characters
+- add basic movement for characters
---
+++ src/main.js
@@ -0,0 +1,28 @@
+//created by cj moshy
+
+'use strict'
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    render: {
+        pixelArt: true
+    },
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            //debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: []
+}
+
+let game = new Phaser.Game(config)
